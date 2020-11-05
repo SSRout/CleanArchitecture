8af9c4771537f1dc235f13a487c70d6549bcc3a5
ssrout <smruti.sambit.rout@gmail.com>
2020-11-06 02:06:07 +0530

    initial skeliton

---
+++ InnoTech.Core.Entity/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace InnoTech.Core.Entity
+{
+    public class Class1
+    {
+    }
+}
---
+++ InnoTech.Core.PrimaryDriver.Adapters.Test/Services/LocationServiceTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InnoTech.Core.PrimaryDriver.Adapters.Test.Services
+{
+    class LocationServiceTest
+    {
+    }
+}
